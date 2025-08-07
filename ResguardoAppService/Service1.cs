a5ab58e73750d84f9013c762aee9d2fa3c7773db
cesjavi <cesjavi@gmail.com>
2025-08-07 00:51:21 -0300

    ff

--- ResguardoAppService/Service1.cs
+++ ResguardoAppService/Service1.cs
@@ -1,4 +1,5 @@
 using System.ServiceProcess;
+using System.Diagnostics;
 
 namespace ResguardoAppService
 {
@@ -7,16 +8,24 @@ namespace ResguardoAppService
         public Service1()
         {
             InitializeComponent();
+            if (!EventLog.SourceExists("ResguardoAppService"))
+            {
+                EventLog.CreateEventSource("ResguardoAppService", "Application");
+            }
         }
 
         protected override void OnStart(string[] args)
         {
+            EventLog.WriteEntry("ResguardoAppService", "Servicio iniciado.", EventLogEntryType.Information);
             // Lógica de respaldo programado
+
             // Podés usar un Timer, leer tu archivo de configuración, etc.
         }
 
         protected override void OnStop()
         {
+                EventLog.WriteEntry("ResguardoAppService", "Servicio detenido.", EventLogEntryType.Information);
+
             // Lógica de limpieza/parada
         }
     }
